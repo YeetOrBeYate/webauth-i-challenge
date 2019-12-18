e8ba37f73d3f8c2cd58e460a10bde8be4015b6e3
Kyle Corbin <kylecorbin26@gmail.com>
2019-12-17 19:16:12 -0500

    Got mvp and a little stretch

--- Login/Login-Router.js
+++ Login/Login-Router.js
@@ -9,6 +9,8 @@ router.post("/", (req,res)=>{
    qs.login(body.username)
    .then(user=>{
        if(user && bcrypt.compareSync(body.password, user.password)){
+           //reqesting that the session now has a user property
+           req.session.user = user;
            res.status(200).json((user))
        }else{
            res.status(401).json({message: 'wrong credentials'})
@@ -17,6 +19,20 @@ router.post("/", (req,res)=>{
    .catch(err=>{
        res.status(500).json({err})
    })
+})
+
+router.get("/logout", (req,res)=>{
+    if(req.session && req.session.user){
+        req.session.destroy(err=>{
+            if(err){
+                res.status(500).json({message: "Error logging you out"})
+            }else{
+                res.status(201).json({message: "GoodBye, succesfully logged out!"})
+            }
+        })
+    }else{
+        res.status(500).json({message: "Can't log you out, maybe you were never in?"})
+    }
 })
 
 module.exports = router;
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const knex = require("./data/db.js");
 
 const RegRouter = require("./Register/reg-Router");
 const LoginR = require("./Login/Login-Router");
+const Restrict = require("./User/restricted.js");
 const app = express();
 
 
@@ -38,9 +39,14 @@ const sessionConfig = {
 app.use(express.json());
 app.use(cors());
 app.use(session(sessionConfig));
-app.use('/users', UserRouter);
+app.use('/users', Restrict, UserRouter);
 app.use('/register',RegRouter);
 app.use('/login', LoginR );
+
+app.get('/session',(req,res)=>{
+    const yeet = req.session;
+    res.status(200).json({yeet})
+})
 
 const port = 8080;
 app.listen(port, ()=>{console.log("yeet on", port)})
